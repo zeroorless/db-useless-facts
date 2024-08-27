627f3dc83611fca094fc2cf67f33d75ab8af7c5f
Grigory Ponomarev <grigory.ponomarev@jetbrains.com>
2024-08-27 22:16:05 +0200

    Base62Encoder small changes

--- src/main/kotlin/utils/Base62Encoder.kt
+++ src/main/kotlin/utils/Base62Encoder.kt
@@ -2,14 +2,22 @@ package gp.example.utils
 
 class Base62Encoder : Encoder {
     companion object {
-        private val alphabet = ('0' .. '9').toList() + ('A'..'z').toList()
+        private val alphabet = ('0' .. '9').toList() + ('A'..'Z').toList() + ('a'..'z').toList()
         private val base = 62
+        const val zero = "0"
     }
 
+    /**
+     * Encodes a long number to a base-62 number
+     * @param n non-negative long number
+     * @return Base-62 encoded string
+     * @throws IllegalArgumentException if n is negative.
+     */
     override fun encode(n: Long): String {
-        var n = n
-        return if (n == 0L) "0"
+        if (n < 0L) throw IllegalArgumentException("n must be non-negative")
+        return if (n == 0L) zero
         else {
+            var n = n
             val accumulator = StringBuilder()
             while (n > 0L) {
                 val c = alphabet[(n % base).toInt()]
